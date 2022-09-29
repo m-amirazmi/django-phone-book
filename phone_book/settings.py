aaddd997108aee6f11dbfb00d40ebb04e402fc41
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-09-29 13:51:47 +0800

    change to production

--- phone_book/settings.py
+++ phone_book/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wx^60@4q5mz43f10h#^rsx#@a51zl9-mbaxj*347x%x=%=&7!$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["$HOST"]
 
 
 # Application definition
